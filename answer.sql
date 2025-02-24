77e19f6102b3212d76d7378a97f6500d5381b020
Luffy4king <101545710+Luffy4king@users.noreply.github.com>
2025-02-24 20:06:58 +0530

    Task 3 Completed

--- answer.sql
+++ answer.sql
@@ -1,7 +1,12 @@
 -- TYPE YOUR SQL QUERY BELOW
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
-
+SELECT
+    DATE(datetime(login_timestamp, 'unixepoch')) AS login_date,
+    COUNT(DISTINCT user_id) AS daily_active_users
+FROM login_history
+GROUP BY login_date
+ORDER BY login_date;
 
 
 
@@ -9,5 +14,15 @@
 
 
 
-
+SELECT 
+    cardID,
+    COUNT(*) AS total_status_changes
+FROM 
+    card_change_history
+WHERE 
+    oldStatus IS NOT NULL  
+GROUP BY 
+    cardID
+ORDER BY 
+    cardID;
 
